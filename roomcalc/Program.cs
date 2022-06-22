4caedc48c029cc0d4b7c16451600269f1cc56186
Jeo <jeo961@gmail.com>
2022-06-22 09:16:13 -0400

    first commit

---
+++ roomcalc/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Welcome to the room calculator.");
+
+//ask for values
+Console.WriteLine("Please enter the length of the room.");
+double length = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the width of the room.");
+double width = double.Parse(Console.ReadLine());
+
+//Calculations
+double area = length * width;
+double perimeter = 2 * (length + width);
+
+//Display Results
+Console.WriteLine("Area:" + area);
+Console.WriteLine("Perimeter:" + perimeter);
